769d1334ca7349dfe401e6ce23d085c329d1d93f
johnnyrich0617 <john.richardson.0617@gmail.com>
2015-01-04 17:35:59 -0500

    Fixed context Scan issue

--- chapter2/spring/src/main/java/camelinaction/FileToJMSRoute.java
+++ chapter2/spring/src/main/java/camelinaction/FileToJMSRoute.java
@@ -4,10 +4,14 @@
 package camelinaction;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FileToJMSRoute extends RouteBuilder {
     @Override
     public void configure() {
-        from("ftp://rider.com/orders?username=rider&password=secret").to("jms:incomingOrders");
+        from("ftp://192.168.1.23/orders?username=FTPUSER1&password=Trinity!0617")
+        .process(new DownloadLogger())
+        .to("jms:incomingOrders");
     }
 }
